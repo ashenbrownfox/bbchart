7a5eaad890331aaba170f4a2843baf8d93f3a61f
ashenbrownfox <newhoopsdynasty@gmail.com>
2020-07-24 16:38:16 -0400

    get rid of incomplete feature

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -19,11 +19,6 @@ const Search = ({ getQuery }) => {
           onChange={(e) => onChange(e.target.value)}
           autoFocus
         />
-         <input type="checkbox" name = "bb"
-        value="bb"/>    <label for="bb">Breaking Bad</label>
-        <br/>
-         <input type="checkbox" name = "bcs"
-        value="bcs"/>    <label for="bcs">Better Call Saul</label>
       </form>
     </section>
   )
