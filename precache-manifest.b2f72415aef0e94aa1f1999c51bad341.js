093a80ec8812a46e8e7840fb86ac96046de08eb7
ashenbrownfox <newhoopsdynasty@gmail.com>
2020-07-24 16:44:10 -0400

    Updates

---
+++ precache-manifest.b2f72415aef0e94aa1f1999c51bad341.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2167f236a67a3ba4b75b26d59b3be888",
+    "url": "/bbchart/index.html"
+  },
+  {
+    "revision": "186017ad656bfd65ecf7",
+    "url": "/bbchart/static/css/main.7ddc275d.chunk.css"
+  },
+  {
+    "revision": "84d09f3a56c36532cbb7",
+    "url": "/bbchart/static/js/2.6f42770e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bbchart/static/js/2.6f42770e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186017ad656bfd65ecf7",
+    "url": "/bbchart/static/js/main.c6269d5e.chunk.js"
+  },
+  {
+    "revision": "97604e5dfa6ca60a904b",
+    "url": "/bbchart/static/js/runtime-main.00d288ed.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/bbchart/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/bbchart/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/bbchart/static/media/spinner.235b9b3e.gif"
+  }
+]);
